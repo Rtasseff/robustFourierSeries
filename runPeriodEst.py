fe081992149d07fb1188b255eb6aceea5a49ee45
rtasseff <rtasseff@gmail.com>
2013-09-13 17:04:48 +0200

    add files for MP GOLEM

---
+++ runPeriodEst.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from fourierSeries import estSigGStat
+import numpy as np
+
+index = int(sys.argv[1])
+tPath = sys.argv[2]
+xPath = sys.argv[3]
+wPath = sys.argv[4]
+perms = int(sys.argv[5])
+damp = float(sys.argv[6])
+#outPath = sys.argv[7]
+
+	
+t = np.loadtxt(tPath)
+X = np.loadtxt(xPath)
+if wPath == 'NA':
+	wV = []
+else:
+	wV = np.loadtxt(wPath)
+
+x = X[index,:]
+
+p,w = estSigGStat(x,t,wV = wV, nPerm=perms,permEst=False,damp=damp)
+if np.isnan(p) or p==0:
+	p,w = estSigGStat(x,t,wV = wV, nPerm=perms*100,permEst=True,damp=damp)
+
+#f = open(outPath,'a')
+#f.write('>>\t'+str(index)+'\t'+str(p)+'\t'+str(w)+'\n')
+
+#f.flush()
+#os.fsync(f)
+#f.close()
+
+print('>>\t'+str(index)+'\t'+str(p)+'\t'+str(w)+'\n')
+
